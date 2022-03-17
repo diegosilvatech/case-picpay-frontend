45ef2a859a83daea5cca4bebde9211e8c5ef392e
coderamos <diego.coderamos@gmail.com>
2022-03-17 14:31:31 -0300

    feat: update icons structure

--- src/assets/icons/ArrowDown/index.tsx
+++ src/assets/icons/ArrowDown/index.tsx
@@ -10,6 +10,7 @@ export const ArrowDownIcon = ({ color }: IconType) => (
       viewBox="0 0 24 24"
       width="24px"
       fill="currentColor"
+      data-testid="arrow-icon"
     >
       <path d="M0 0h24v24H0V0z" fill="none" />
       <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
