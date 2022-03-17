ed488e5d21f0a94cb3a38a456fae071119b7b614
coderamos <diego.coderamos@gmail.com>
2022-03-17 20:56:52 -0300

    feat: update LoginPage layout

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
+
 import { Container, FormLogin } from 'components';
 
 import * as s from './styles';
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('submit');
+    console.log('submit', { email, password });
+  };
+
+  const formData = {
+    email,
+    setEmail,
+    password,
+    setPassword
   };
   return (
     <s.Wrapper>
       <Container>
         <s.FormWrapper>
-          <FormLogin onSubmit={onSubmit} />
+          <FormLogin onSubmit={onSubmit} data={formData} />
         </s.FormWrapper>
       </Container>
     </s.Wrapper>
