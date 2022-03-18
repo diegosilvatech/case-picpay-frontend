bce2131e7855301b8b2f0af49bb83b13dcbc1062
coderamos <diego.coderamos@gmail.com>
2022-03-18 14:43:09 -0300

    feat: update pages layout

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+
+import { AuthContext } from 'contexts';
 
 import { Container, FormLogin } from 'components';
 
 import * as s from './styles';
 
 export default function LoginPage() {
+  const { authenticated, login } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('submit', { email, password });
+    if (email !== '' && password !== '') {
+      login(email, password);
+    }
   };
 
   const formData = {
@@ -23,6 +28,7 @@ export default function LoginPage() {
     <s.Wrapper>
       <Container>
         <s.FormWrapper>
+          <p>LOGADO? {`${authenticated}`} </p>
           <FormLogin onSubmit={onSubmit} data={formData} />
         </s.FormWrapper>
       </Container>
--- src/pages/PaymentsPage/index.tsx
+++ src/pages/PaymentsPage/index.tsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
+
+import { AuthContext } from 'contexts';
+
 import { Button, Container } from 'components';
 
 import * as s from './styles';
 
 export default function PaymentsPage() {
+  const { logout } = useContext(AuthContext);
+
   return (
     <s.Wrapper>
       <Container>
-        <Button onClick={() => alert('welcome!')}>Payments/</Button>
+        <Button onClick={() => logout()}>LOGOUT/</Button>
       </Container>
     </s.Wrapper>
   );
