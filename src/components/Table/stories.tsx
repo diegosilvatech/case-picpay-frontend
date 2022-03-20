cd588129632b776050e50d635ac55a27fa2ee74e
coderamos <diego.coderamos@gmail.com>
2022-03-20 15:52:48 -0300

    feat: update table story

--- src/components/Table/stories.tsx
+++ src/components/Table/stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import Table, { TableProps } from '.';
@@ -11,8 +12,28 @@ export default {
     jest: ['Table.test.tsx']
   },
   args: {
+    currentPage: 1,
+    pageSize: 5,
+    total: mockedTableData.length,
+    handlePageChange: () => ({}),
     data: mockedTableData
   }
 } as Meta;
 
-export const TableDefault: Story<TableProps> = (args) => <Table {...args} />;
+export const TableDefault: Story<TableProps> = (args) => {
+  const [currentPage, setCurrentPage] = useState<number>(args.currentPage);
+  const [pageSize, setPageSize] = useState<number>(args.pageSize);
+
+  const handlePageChange = (currentPage: number, pageSize: number) => {
+    setCurrentPage(currentPage), setPageSize(pageSize);
+  };
+
+  return (
+    <Table
+      {...args}
+      handlePageChange={handlePageChange}
+      pageSize={pageSize}
+      currentPage={currentPage}
+    />
+  );
+};
